aa64749a78133b35650b59e1f43f72f83da61dd0
Pedro Javier <thepetermail@gmail.com>
2017-04-07 21:42:08 +0200

    Update source code.
    Include License.

--- src/netw.h
+++ src/netw.h
@@ -1,4 +1,22 @@
-//Rev 0.2
+/*
+	netw.h, rev 0.4, 07/04/2017 
+	high level cross-platform network wrapper
+    Copyright (C) 2017  pedro-javierf
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef INC_NETW_H //Macro protection
 #define INC_NETW_H
 
